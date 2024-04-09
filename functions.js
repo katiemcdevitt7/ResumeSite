8091a731cdce68aa4737bec6276b1e7332c656ad
Katie McDevitt <katie.mcdevitt7@gmail.com>
2024-04-09 18:44:54 -0400

    added java script to hide side bar for the smaller screens

--- functions.js
+++ functions.js
@@ -1,3 +1,16 @@
 function navigateToPage(url) {
     window.location.href = url; // Replace 'another-page.html' with the URL of the desired file
 }
+const toggleBtn = document.getElementById('toggleBtn');
+const sidebar = document.getElementById('SideBar');
+const mainContent = document.getElementById('MainContent');
+
+toggleBtn.addEventListener('click', () => {
+    if (sidebar.style.width === '0px' || sidebar.style.width === '') {
+        sidebar.style.width = '250px';
+        mainContent.style.marginLeft = '250px';
+    } else {
+        sidebar.style.width = '0';
+        mainContent.style.marginLeft = '0';
+    }
+});
