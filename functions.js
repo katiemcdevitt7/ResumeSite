fd124148f9a4d8ac965520dd572d5b727273bd9a
Katie McDevitt <katie.mcdevitt7@gmail.com>
2024-04-09 20:38:24 -0400

    been fixing sizing for smaller screen layout

--- functions.js
+++ functions.js
@@ -1,16 +1,35 @@
 function navigateToPage(url) {
-    window.location.href = url; // Replace 'another-page.html' with the URL of the desired file
+    window.location.href = url;
 }
-const toggleBtn = document.getElementById('toggleBtn');
-const sidebar = document.getElementById('SideBar');
-const mainContent = document.getElementById('MainContent');
 
-toggleBtn.addEventListener('click', () => {
-    if (sidebar.style.width === '0px' || sidebar.style.width === '') {
-        sidebar.style.width = '250px';
-        mainContent.style.marginLeft = '250px';
-    } else {
-        sidebar.style.width = '0';
-        mainContent.style.marginLeft = '0';
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const SideBar = document.getElementById('SideBar');
+    const MainContent = document.getElementById('MainContent');
+    const menubutton = document.getElementById('menubutton');
+    const menubutton2 = document.getElementById('menubutton2');
+    const menubutton3 = document.getElementById('menubutton3');
+    const menubutton4 = document.getElementById('menubutton4');
+    const menubutton5 = document.getElementById('menubutton5');
+
+    console.log(toggleBtn, SideBar, MainContent);  
+    toggleBtn.addEventListener('click', () => {
+        if (SideBar.style.width === '0px' || SideBar.style.width === '') {
+            SideBar.style.width = '20%';
+            menubutton.style.display = 'inline-block'
+            menubutton2.style.display = 'inline-block'
+            menubutton3.style.display = 'inline-block'
+            menubutton4.style.display = 'inline-block'
+            menubutton5.style.display = 'inline-block'
+
+        } else {
+            SideBar.style.width = '0';
+            menubutton.style.display = 'none'
+            menubutton2.style.display = 'none'
+            menubutton3.style.display = 'none'
+            menubutton4.style.display = 'none'
+            menubutton5.style.display = 'none'
+
+        }
+    });
 });
